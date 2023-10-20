356c09c75a79b676869ccdb21eca3e721782ad70
lyu-chunkwo <lyu.chun.kwo@gmail.com>
2023-10-20 15:35:21 +0600

    add sertifications section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function () {
-  
+  const menuBtn = document.querySelector('.menu__btn');
+  const menuMobile = document.querySelector('.menu__list');
+
+  menuBtn.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('menu--open');
+  })
+
+
+  const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
+
+
+  $('.sertificates__slider').slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots: true,
+    variableWidth: true
+  })
+
 });
